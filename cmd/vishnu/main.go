02c869e53ff6b28ec8cf7862753ec366532d82ba
Stan Zhai <zhaishidan@haizhi.com>
2018-09-21 16:05:43 +0800

    first commit

---
+++ cmd/vishnu/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+
+	reuse "github.com/libp2p/go-reuseport"
+)
+
+func main() {
+	l, err := reuse.Listen("tcp", "127.0.0.1:1234")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+	for {
+		// Wait for a connection.
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Handle the connection in a new goroutine.
+		// The loop then returns to accepting, so that
+		// multiple connections may be served concurrently.
+		go func(c net.Conn) {
+			// Echo all incoming data.
+			log.Printf("client addr: %s", c.RemoteAddr())
+			//c2, e := net.Dial("tcp", "localhost:1235", "localhost:5000")
+			c2, e := net.Dial("tcp", "localhost:5000")
+			if e != nil {
+				log.Fatal(e)
+				return
+			}
+			d := make(chan int, 1)
+			go func() {
+				io.Copy(c2, c)
+				d <- 1
+			}()
+			go func() {
+				io.Copy(c, c2)
+				d <- 1
+			}()
+			// Shut down the connection.
+			<-d
+			<-d
+			c2.Close()
+			c.Close()
+		}(conn)
+	}
+}
